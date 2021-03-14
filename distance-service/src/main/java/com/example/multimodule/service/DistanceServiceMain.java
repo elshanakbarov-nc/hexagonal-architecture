e170ccc2b713bf59b022a53278dfa9e79a4b32a5
elshanakberov <elshanakbarovv@gmail.com>
2021-03-14 13:44:05 +0400

    Initial commit

---
+++ distance-service/src/main/java/com/example/multimodule/service/DistanceServiceMain.java
@@ -0,0 +1,11 @@
+package com.example.multimodule.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistanceServiceMain {
+    public static void main(String[] args) {
+        SpringApplication.run(DistanceServiceMain.class, args);
+    }
+}
---
+++ route-service/src/main/java/com/example/multimodule/application/RouteServiceMain.java
@@ -0,0 +1,11 @@
+package com.example.multimodule.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteServiceMain {
+    public static void main(String[] args) {
+        SpringApplication.run(RouteServiceMain.class,args);
+    }
+}
