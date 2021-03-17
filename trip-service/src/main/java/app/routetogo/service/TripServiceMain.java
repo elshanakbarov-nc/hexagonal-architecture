261b97817da6efe4bd1f490c51b61037f432ef71
elshanakberov <elshanakbarovv@gmail.com>
2021-03-17 22:37:34 +0400

    Add trip service

---
+++ trip-service/src/main/java/app/routetogo/service/TripServiceMain.java
@@ -0,0 +1,11 @@
+package app.routetogo.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripServiceMain {
+    public static void main(String[] args) {
+        SpringApplication.run(TripServiceMain.class,args);
+    }
+}
